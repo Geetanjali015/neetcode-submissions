80ae769c703aff3a8eb5aef774f0bf00c8f780cc
Geetanjali Reddy <142789279+Geetanjali015@users.noreply.github.com>
2026-06-04 00:40:18 +0530

    comments

--- Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cpp
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.empty()) return 0;
-        int i=1;
+        if(nums.empty()) return 0; 
+        //i=writer, j=scanner
+        int i=1; 
         for(int j=1;j<nums.size();j++){
-            if(nums[j]!=nums[j-1]){
-                nums[i]=nums[j];
-                i++;
+            if(nums[j]!=nums[j-1]){ // 1 1 2 3 4 //2!=1
+                nums[i]=nums[j]; //write value at pos j in pos i  // 1 2 2 3 4 // 2->1
+                i++;// and inc i to nect element
             }
        }
         return i;
     }
 };
+
+//tc=o(n) , sc=o(1)
