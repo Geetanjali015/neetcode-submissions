c82d5ff7035335308ca79c055051cd5375df3373
Geetanjali Reddy <142789279+Geetanjali015@users.noreply.github.com>
2026-06-19 15:25:29 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxR=0;
+        for(int i=0;i<nums.size();i++){
+            if(i>maxR) return false;
+            maxR=max(maxR,i+nums[i]);  //curr pos+jump. length
+
+
+        }
+        return true;
+        
+    }
+};
