b122735e45a560cc709825bf7ee644a28a69e1f7
Geetanjali Reddy <142789279+Geetanjali015@users.noreply.github.com>
2026-06-19 15:37:14 +0530

    Add: non-overlapping-intervals - submission-0

---
+++ Data Structures & Algorithms/non-overlapping-intervals/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {// intervals are sorted by end time
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        int n=intervals.size();
+        int count=0;
+        int prevEnd=intervals[0][1];
+        for(int i=1;i<n;i++){
+            int currStart=intervals[i][0];
+            if(currStart<prevEnd){//overlap
+                count++;
+                prevEnd=min(prevEnd,intervals[i][1]); //keep the one which ends earlier
+            }
+            else{
+                prevEnd=intervals[i][1];
+            }
+
+        }
+        return count;
+
+
+
+
+        
+    }
+};
