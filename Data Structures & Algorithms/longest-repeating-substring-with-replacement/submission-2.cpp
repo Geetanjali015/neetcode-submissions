b86c9f22295f849732f714266d081c135fd29142
Geetanjali Reddy <142789279+Geetanjali015@users.noreply.github.com>
2026-08-13 21:34:55 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,20 @@
+class Solution { //the highest frequency we have seen in the window as it grows (maxf), does not loop throughh every character c {A,B}, only the max one
+public:
+    int characterReplacement(string s, int k) {
+       unordered_map<char,int>mp;
+       int ans=0;
+       int l=0,mf=0;
+       for(int r=0;r<s.size();r++){
+        mp[s[r]]++;
+        mf=max(mf,mp[s[r]]);
+        while((r-l+1)-mf>k){
+            mp[s[l]]--;
+            l++;
+        }
+        ans=max(ans,r-l+1);
+       }
+       return ans;
+
+        
+    }
+};
